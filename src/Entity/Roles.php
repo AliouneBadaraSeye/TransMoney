84a3e4772be55b4ca8d871fc199b4e3a2f829510
AliouneBadaraSeye <lomoubaye@gmail.com>
2020-02-18 13:32:18 +0000

    Reprise du Projet de transMonoey depuis le début

---
+++ src/Entity/Roles.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\RolesRepository")
+ */
+class Roles
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="UserRole")
+     */
+    private $Users;
+
+    public function __construct()
+    {
+        $this->Users = new ArrayCollection();
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->Users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->Users->contains($user)) {
+            $this->Users[] = $user;
+            $user->setUserRole($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->Users->contains($user)) {
+            $this->Users->removeElement($user);
+            // set the owning side to null (unless already changed)
+            if ($user->getUserRole() === $this) {
+                $user->setUserRole(null);
+            }
+        }
+
+        return $this;
+    }
+
+}  
